e2bfc297cf3beb401a095eabd57d6311867d5a26
wellintonmbvix <wellinton.m.b.vix@bol.com.br>
2022-08-02 06:43:01 -0300

    Alterando layout

--- packages/front-end/src/components/AppLayout/index.tsx
+++ packages/front-end/src/components/AppLayout/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Head from '../Head';
+import Footer from '../Footer';
 
-export default function AppLayout() {
+export default function AppLayout({ children }: { children: JSX.Element}) {
   return (
-    <div>AppLayout</div>
+    <div className="w-full h-screen min-h-640 flex flex-nowrap flex-col overflow-hidden bg-gray-200 dark:bg-gray-900">
+      <Head />
+      {children}
+      <Footer />
+    </div>
   )
 }
---
+++ packages/front-end/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="p-4 bg-gray-100 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-700">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2022{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            Flowbite™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </footer>
+    </>
+  );
+}
